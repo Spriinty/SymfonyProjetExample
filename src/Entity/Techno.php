3dbc9f321555a2e9eedee9ee2a73cf3d52cb70e4
Spriinty <quentinpetrequin@gmail.com>
2020-02-13 08:13:46 +0000

    Ajout sécurité formulaire techno

--- src/Entity/Techno.php
+++ src/Entity/Techno.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TechnoRepository")
@@ -20,6 +22,7 @@ class Techno
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez remplir le champ ci-dessous ↓")
      */
     private $name;
 
